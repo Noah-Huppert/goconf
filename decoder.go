2b40feef58b42402bb7d1cb8c15717c4e988a5fd
Noah Huppert <contact@noahh.io>
2019-03-11 02:08:24 -0400

    made MapDecoder.Decode return an error

--- decoder.go
+++ decoder.go
@@ -7,5 +7,5 @@ import (
 // MapDecoder decodes a Reader into a map
 type MapDecoder interface {
 	// Decode a reader into a map
-	Decode(r io.Reader, m *map[string]interface{})
+	Decode(r io.Reader, m *map[string]interface{}) error
 }
