c959dd56e5b48d6821b55d54e66a49dfc2ef1c21
Noah Huppert <contact@noahh.io>
2019-03-11 00:55:02 -0400

    defined MapDecoder

---
+++ decoder.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"io"
+)
+
+// MapDecoder decodes a Reader into a map
+type MapDecoder interface {
+	// Decode a reader into a map
+	Decode(r io.Reader, m *map[string]interface{})
+}
