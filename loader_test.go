c4fc9ae48efcc14bf13d9bdafda9130ccdf0e8c1
Noah Huppert <contact@noahh.io>
2019-03-11 18:59:24 -0400

    added test to ensure validator runs on result struct

--- loader_test.go
+++ loader_test.go
@@ -22,22 +22,68 @@ func (d DummyMapDecoder) Decode(r io.Reader, m *map[string]interface{}) error {
 	return nil
 }
 
-var configFilesTxt map[string][]byte = map[string][]byte{
+// tempFilesTxt holds the contents of temporary files which will be created for
+// testing purposes. Keys are arbitrary IDs which are used to refer to these
+// temporary files in the future.
+var tempFilesTxt map[string][]byte = map[string][]byte{
 	"a": []byte("key1 = \"value1\""),
 	"b": []byte("key2 = \"value2\""),
 	"c": []byte("key3 = \"value3\""),
 }
 
+// configFile is a dummy configuration file struct definition passed to
+// Loader.Load in a few tests
 type configFile struct {
-	Key1 string `mapstructure:"key1"`
-	Key2 string `mapstructure:"key2"`
-	Key3 string `mapstructure:"key3"`
+	Key1 string `mapstructure:"key1" validate:"required"`
+	Key2 string `mapstructure:"key2" validate:"required"`
+	Key3 string `mapstructure:"key3" validate:"required"`
 }
 
+// expectedConfigFile holds the values which Loader.Load should place in a
+// configFile struct when called
 var expectedConfigFile configFile = configFile{
 	Key1: "value1",
 	Key2: "value2",
 	Key3: "value3",
+}
+
+// placeTempFiles creates temporary files for testing purposes. The names
+// argumen can only include keys which are present in the tempFilesTxt map.
+func placeTempFiles(t *testing.T, names []string) map[string]*os.File {
+	// Place temp files
+	files := map[string]*os.File{}
+
+	for _, name := range names {
+		path := fmt.Sprintf("/tmp/goconf-config-%s.toml", name)
+		f, err := os.Create(path)
+
+		assert.NilError(t, err, "failed to open config file %s", name)
+
+		files[name] = f
+	}
+
+	for name, f := range files {
+		_, err := f.Write(tempFilesTxt[name])
+		assert.NilError(t, err, "failed to write to config file %s",
+			name)
+	}
+
+	return files
+}
+
+// cleanupTempFiles removes temp files provided by PlaceTempFiles. The files
+// argument should be the map returned by placeTempFiles.
+func cleanupTempFiles(t *testing.T, files map[string]*os.File) {
+	for name, f := range files {
+		assert.NilError(t, f.Close(), "failed to close config"+
+			"file %s", name)
+	}
+
+	for name, _ := range files {
+		path := fmt.Sprintf("/tmp/goconf-config-%s.toml", name)
+		assert.NilError(t, os.Remove(path), "failed to remove"+
+			"config file %s", name)
+	}
 }
 
 // TestRegisterFormat ensures MapDecoders are properly added to the formats map
@@ -70,37 +116,8 @@ func TestAddConfigPath(t *testing.T) {
 // TestLoad ensures Load reads and decodes configuration files
 func TestLoad(t *testing.T) {
 	// Place temp files
-	files := map[string]*os.File{}
-
-	for _, name := range []string{"a", "b", "c"} {
-		path := fmt.Sprintf("/tmp/goconf-config-%s.toml", name)
-		f, err := os.Create(path)
-
-		assert.NilError(t, err, "failed to open config file %s", name)
-
-		files[name] = f
-	}
-
-	for name, f := range files {
-		_, err := f.Write(configFilesTxt[name])
-		assert.NilError(t, err, "failed to write to config file %s",
-			name)
-	}
-
-	defer func() {
-		for name, f := range files {
-			assert.NilError(t, f.Close(), "failed to close config"+
-				"file %s", name)
-		}
-	}()
-
-	defer func() {
-		for name, _ := range files {
-			path := fmt.Sprintf("/tmp/goconf-config-%s.toml", name)
-			assert.NilError(t, os.Remove(path), "failed to remove"+
-				"config file %s", name)
-		}
-	}()
+	tmpFiles := placeTempFiles(t, []string{"a", "b", "c"})
+	defer cleanupTempFiles(t, tmpFiles)
 
 	// Setup loader
 	loader := NewLoader()
@@ -116,4 +133,26 @@ func TestLoad(t *testing.T) {
 	assert.NilError(t, err, "failed to load configuration")
 
 	assert.DeepEqual(t, actualConfig, expectedConfigFile)
+}
+
+// TestLoadValidate ensures Load validates after reading files into a struct
+func TestLoadValidate(t *testing.T) {
+	// Place temp files
+	tmpFiles := placeTempFiles(t, []string{"a", "b"})
+	defer cleanupTempFiles(t, tmpFiles)
+
+	// Setup loader
+	loader := NewLoader()
+
+	loader.AddConfigPath("/tmp/goconf-config-*.toml")
+	loader.RegisterFormat(".toml", toml.TomlMapDecoder{})
+
+	// Loader
+	actualConfig := configFile{}
+
+	err := loader.Load(&actualConfig)
+
+	assert.Equal(t, err.Error(), "failed to validate configuration "+
+		"struct: Key: 'configFile.Key3' Error:Field validation for "+
+		"'Key3' failed on the 'required' tag")
 }
