ffa69ad04b0994eef38b11a4106c04a1cf3268dc
Noah Huppert <contact@noahh.io>
2019-03-11 04:40:42 -0400

    added intro doc text for godoc

---
+++ doc.go
@@ -0,0 +1,7 @@
+package main
+
+/// Goconf is a simple, straightforward go configuration library.
+///
+/// Configuration is defined via structs and tags. Values are loaded from files.
+///
+/// Any file format can be used.
