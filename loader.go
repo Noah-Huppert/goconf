d86fa786869474ccf8f73684b4b897f04a027249
Noah Huppert <contact@noahh.io>
2019-03-11 01:02:53 -0400

    implemented util methods for Loader

--- loader.go
+++ loader.go
@@ -8,3 +8,29 @@ type Loader struct {
 	// configPaths are paths to files to load
 	configPaths []string
 }
+
+// NewLoader creates a Loader
+func NewLoader() {
+	return Loader{
+		formats:     map[string]MapDecoder{},
+		configPaths: []string{},
+	}
+}
+
+// RegisterFormat adds a MapDecoder to the formats field
+func (l *Loader) RegisterFormat(ext string, decoder MapDecoder) {
+	l.formats[ext] = decoder
+}
+
+// AddConfigPath adds a path to the configPaths field
+func (l *Loader) AddConfigPath(p string) {
+	// Check if already in configPaths
+	for existingPath := range l.configPaths {
+		if existingPath == p {
+			return
+		}
+	}
+
+	// Add
+	l.configPaths = append(l.configPaths, p)
+}
